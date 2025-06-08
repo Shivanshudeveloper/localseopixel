c9ace1adacbaa32a4d7c77acff6cdc6e97254184
Shivanshu Gupta <40379768+Shivanshudeveloper@users.noreply.github.com>
2025-06-08 21:02:20 +0530

    Changes

--- pixel.js
+++ pixel.js
@@ -74,7 +74,7 @@
                 ip: null
             };
             
-            fetch("http://localhost:4000/api/track", {
+            fetch("http://localhost:8080/api/v1/service/track", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(trackingDataNoIP),
